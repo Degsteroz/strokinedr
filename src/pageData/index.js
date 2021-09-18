a74df74fbf28011722bf49b72d369393200faf30
Vitaly O <degsteroz@gmail.com>
2021-09-18 23:08:28 +0500

    init

---
+++ src/pageData/index.js
@@ -0,0 +1,4 @@
+export const info ={
+    imageLink: 'https://sun9-87.userapi.com/impg/OmpUWNMBtky9XBBh4QVc76NnLqlzVtcPl2sWyw/op3WfOzfLD8.jpg?size=400x600&quality=96&sign=7e5ff7b4e0f0bcdb88606bc30f9cd49b&type=album',
+    imageLink2: 'https://sun9-53.userapi.com/impg/DBVdlY1UaSDTbyCOuYySpEl2OmSTkEEAUUv6-Q/18BzSNePkWo.jpg?size=900x600&quality=96&sign=a3a9d5c749eb20769edfbe39069bf370&type=album',
+}
